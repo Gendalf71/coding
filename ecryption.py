c621910a0cbfc297cbb38d35620f276b8339b25e
Gendalf71 <gdedenko@gmail.com>
2024-12-23 21:37:05 +0300

    vinizer

--- ecryption.py
+++ ecryption.py
@@ -1,17 +1,40 @@
-def encrypt(k, m):
-    return ''.join(map(chr, [(ord(c) + k) % 65536 for c in m]))
+# Функция для размножения ключа до длины сообщения
+def extend_key(key, length):
+    repeated_key = (key * (length // len(key) + 1))[:length]
+    return repeated_key
 
-def decrypt(k, m):
-    return ''.join(map(chr, [(ord(c) - k) % 65536 for c in m]))
+# Функция шифрования шифром Виженера
+def encrypt(key, message):
+    # Размножаем ключ до длины сообщения
+    extended_key = extend_key(key, len(message))
+    # Шифруем каждый символ
+    encrypted_chars = []
+    for m_char, k_char in zip(message, extended_key):
+        encrypted_char = chr((ord(m_char) + ord(k_char)) % 65536)
+        encrypted_chars.append(encrypted_char)
+    return ''.join(encrypted_chars)
 
-# Запрос ключа и текста от пользователя
-key = int(input("Введите ключ для шифрования (целое число): "))
-text = input("Введите текст для шифрования: ")
+# Функция дешифрования шифра Виженера
+def decrypt(key, ciphertext):
+    # Размножаем ключ до длины шифротекста
+    extended_key = extend_key(key, len(ciphertext))
+    # Дешифруем каждый символ
+    decrypted_chars = []
+    for c_char, k_char in zip(ciphertext, extended_key):
+        decrypted_char = chr((ord(c_char) - ord(k_char)) % 65536)
+        decrypted_chars.append(decrypted_char)
+    return ''.join(decrypted_chars)
 
-# Шифрование текста
-encrypted_text = encrypt(key, text)
-print("Зашифрованный текст:", encrypted_text)
+# Основная часть программы
+if __name__ == "__main__":
+    # Запрашиваем у пользователя ключ и текст для шифрования
+    key = input("Введите ключ для шифрования (строка): ")
+    message = input("Введите текст для шифрования: ")
 
-# Дешифрование текста
-decrypted_text = decrypt(key, encrypted_text)
-print("Расшифрованный текст:", decrypted_text)
+    # Шифруем текст
+    encrypted_text = encrypt(key, message)
+    print("\nЗашифрованный текст:\n", encrypted_text)
+
+    # Дешифруем текст
+    decrypted_text = decrypt(key, encrypted_text)
+    print("\nРасшифрованный текст:\n", decrypted_text)
